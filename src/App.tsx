f8b2e397c187be0fe408fa8ea54d01ae7f12ae8a
boluwatife <owoade.adedamola@yahoo.com>
2020-06-08 15:25:41 +0100

    main search section mainframe

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,15 @@ const App:React.FC = () => {
           <a className="header__btn" href="/#">Give it a try</a>
         </div>
       </header>
-      <div className="row">
-      </div>
+      <main className="main">
+        <div className="row">
+          <h2 className="main__row--text">Select Search Radius</h2>
+          <select className="main__row--radius">
+            <option value="5000">5km</option>
+          </select>
+          <button className="main__btn--search">Search</button>
+        </div>
+      </main>
     </>
   )
 }
