1fc82836f0336ff66198abe47c2513d407a869f3
boluwatife <owoade.adedamola@yahoo.com>
2020-06-08 14:06:08 +0100

    complete header section

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,19 @@ import './App.css';
 
 const App:React.FC = () => {
   return (
-    <div>Hello</div>
+    <>
+      <header className="header">
+        <div className="header__text">
+          <h1 className="header__primary">
+            Hospital Locator.<br></br>
+            <span className="header__secondary">Locate Nearby Hospitals Close to Your Location</span>
+          </h1>
+          <a className="header__btn" href="/#">Give it a try</a>
+        </div>
+      </header>
+      <div className="row">
+      </div>
+    </>
   )
 }
 
